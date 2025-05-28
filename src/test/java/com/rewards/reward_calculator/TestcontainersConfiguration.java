5685d4029b1a31f2a177d845960f1ffa41def96d
theakashshrivastava <76689530+theakashshrivastava@users.noreply.github.com>
2025-05-28 13:13:29 +0530

    Delete src/test/java/com/rewards/reward_calculator/TestcontainersConfiguration.java

--- src/test/java/com/rewards/reward_calculator/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.rewards.reward_calculator;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
